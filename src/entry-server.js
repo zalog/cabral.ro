87c70457684303b90b44a5fa98646f5f3c5e72f2
Catalin Zalog <c@zalog.ro>
2019-02-20 07:33:28 +0200

    feat: gets `store.state` also in ssr context

--- src/entry-server.js
+++ src/entry-server.js
@@ -1,19 +1,21 @@
 import { createApp } from './main';
 
-export default context => {
-  return new Promise((resolve, reject) => {
-    const { app, router } = createApp();
+export default context => new Promise((resolve, reject) => {
+  const { app, router, store } = createApp();
 
-    router.push(context.url);
+  router.push(context.url);
 
-    router.onReady(() => {
-      const matchedComponents = router.getMatchedComponents();
+  router.onReady(() => {
+    const matchedComponents = router.getMatchedComponents();
 
-      if (!matchedComponents.length) {
-        return reject({ code: 404 });
-      }
+    if (!matchedComponents.length) return reject({ code: 404 });
 
+    Promise.all(matchedComponents.map(({ asyncData }) => asyncData && asyncData({
+      store,
+      route: router.currentRoute
+    }))).then(() => {
+      context.state = store.state;
       resolve(app);
-    }, reject);
-  });
-};
+    }).catch(reject);
+  }, reject);
+});
