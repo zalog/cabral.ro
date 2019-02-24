2ac8e741263d78bc2ed0c0a76dd73419310b1a9c
Catalin Zalog <c@zalog.ro>
2019-02-24 19:54:39 +0200

    feat: client initialize store state from server

--- src/entry-client.js
+++ src/entry-client.js
@@ -1,6 +1,11 @@
 import { createApp } from './app';
 
-const { app, router } = createApp();
+const { app, router, store } = createApp();
+
+if (window.__INITIAL_STATE__) {
+  // initialize store state with data injected from server
+  store.replaceState(window.__INITIAL_STATE__);
+}
 
 router.onReady(() => {
   app.$mount('#app');
