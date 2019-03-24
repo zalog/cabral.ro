8a93469b41da4568a1bb7c1ad04802c1b4a959e1
Catalin Zalog <c@zalog.ro>
2019-03-24 12:14:04 +0200

    build: implements vue-hackernews ssr
    https://github.com/vuejs/vue-hackernews-2.0

---
+++ build/webpack.base.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const webpack = require('webpack');
+const { VueLoaderPlugin } = require('vue-loader');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: isProd ? 'production' : 'development',
+  devtool: isProd
+    ? false
+    : '#cheap-module-source-map',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/dist/',
+    filename: '[name].[chunkhash].js'
+  },
+  resolve: {
+    alias: {
+      'public': path.resolve(__dirname, '../public')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          compilerOptions: {
+            preserveWhitespace: false
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  performance: {
+    hints: false
+  },
+  plugins: isProd
+    ? [
+      new VueLoaderPlugin(),
+      new webpack.optimize.ModuleConcatenationPlugin()
+    ]
+    : [
+      new VueLoaderPlugin()
+    ]
+};
