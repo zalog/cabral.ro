504e159203f16315ccb64e842a082773c69df0b3
Catalin Zalog <c@zalog.ro>
2019-03-24 12:25:32 +0200

    build: removes @vue/cli-service

--- scripts/start.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-
-const server = require('../server/index');
-
-const port = process.env.PORT || 8080;
-
-server.listen(port, () => {
-  console.log(`server started at localhost:${port}`);
-});
