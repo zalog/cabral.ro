fa23732d2ce6c271d6d0e6620daf8e2875cf416f
Catalin Zalog <c@zalog.ro>
2019-03-27 23:04:51 +0200

    feat(api): limits `wp/v2/posts` endpoint payload

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,5 @@ const API_HOST = 'https://www.cabral.ro/wp-json/';
 
 export const API = {
   MENU: API_HOST + 'custom/menu',
-  POSTS: API_HOST + 'wp/v2/posts'
+  POSTS: API_HOST + 'wp/v2/posts?fields=title'
 };
