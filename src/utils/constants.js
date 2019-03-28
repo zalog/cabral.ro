1cc8ac96606aad90406d36bc8ec20b2e5fb95bc4
Catalin Zalog <c@zalog.ro>
2019-03-28 23:22:11 +0200

    feat: switch to js template literals

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 const API_HOST = 'https://www.cabral.ro/wp-json/';
 
 export const API = {
-  MENU: API_HOST + 'custom/menu',
-  POSTS: API_HOST + 'wp/v2/posts?fields=title'
+  MENU: `${API_HOST}custom/menu`,
+  POSTS: `${API_HOST}wp/v2/posts?fields=title`
 };
