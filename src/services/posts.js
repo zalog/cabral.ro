557659d5bebf195cf93587cc2466757ed00bf8a2
Catalin Zalog <c@zalog.ro>
2019-03-30 12:59:41 +0200

    feat(services): improves fetchPosts arguments flow

--- src/services/posts.js
+++ src/services/posts.js
@@ -1,17 +1,28 @@
 import Vue from 'vue';
 import { API } from './../utils/constants';
 
-export function fetchPosts({
-  fields = [],
-  itemsOnPage = 10,
-  currentPage = 1,
-  categories = []
-} = {}) {
-  let params = {};
-  fields.length && ( params['fields'] = fields.join(',') );
-  itemsOnPage && ( params['per_page'] = itemsOnPage );
-  currentPage && ( params['page'] = currentPage );
-  categories.length && ( params['filter[category_name]'] = categories.join(',') );
+export function fetchPosts(payload) {
+  let params = {
+    fields: [],
+    itemsOnPage: 10,
+    currentPage: 1,
+    categories: [],
+    ...payload
+  };
+
+  // prepare params data values
+  params.fields.length && ( params.fields = params.fields.join(',') );
+  params.categories.length && ( params.categories = params.categories.join(',') );
+
+  // renames params keys to match wp-api
+  Object.entries({
+    'itemsOnPage': 'per_page',
+    'currentPage': 'page',
+    'categories': 'filter[category_name]'
+  }).forEach(entry => {
+    params[entry[1]] = params[entry[0]];
+    delete params[entry[0]];
+  });
 
   return new Promise((resolve, reject) => {
     Vue.prototype.$http({
