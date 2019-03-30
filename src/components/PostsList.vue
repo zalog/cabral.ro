284ee2a944bc0b6f53abe4dbe3ece72b9bfd4344
Catalin Zalog <c@zalog.ro>
2019-03-30 07:58:11 +0200

    feat: adds PostsList component

---
+++ src/components/PostsList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div>Posts</div>
+    <ul>
+      <li v-for="(post, index) in posts" :key="index">
+        <a v-html="post.title.rendered" />
+      </li>
+    </ul>
+
+    <span v-for="(page, index) in pagination" :key="index">
+      <router-link
+        :to="'/page/' + page"
+        @click.native="fetchPosts(page)"
+      >{{ page }}</router-link>
+      /
+    </span>
+  </div>
+</template>
+
+<script>
+import postsModule from '../store/modules/posts';
+
+export default {
+  name: 'PageHome',
+
+  computed: {
+    posts() {
+      return this.$store.state.posts.posts;
+    },
+    pagination() {
+      return this.$store.state.posts.pagination;
+    }
+  },
+
+  serverPrefetch() {
+    this.registerModule();
+    return this.fetchPosts(this.$route.params.id);
+  },
+
+  beforeMount() {
+    this.registerModule();
+    if (!this.$store.state.posts.posts.length) this.fetchPosts(this.$route.params.id);
+  },
+
+  destroyed() {
+    this.$store.unregisterModule('posts');
+  },
+
+  watch: {
+    $route(to) {
+      if (typeof to.params.id === 'undefined') this.fetchPosts(1);
+    }
+  },
+
+  methods: {
+    registerModule() {
+      this.$store.registerModule('posts', postsModule, { preserveState: !!this.$store.state.posts });
+    },
+    fetchPosts(pageNr) {
+      return this.$store.dispatch('posts/fetch', pageNr);
+    }
+  }
+};
+</script>
