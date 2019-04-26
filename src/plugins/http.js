d6a7d8387177ac2c6c18c25f0b4e2a5423a3438e
Catalin Zalog <c@zalog.ro>
2019-04-26 10:45:13 +0300

    feat: integrates nprogress
    To do: trigger `NProgress.start()` only when we load a new page.
    Now it triggers on any axios call.

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -1,4 +1,20 @@
 import axios from 'axios';
+import NProgress from 'nprogress';
+
+NProgress.configure({
+  parent: '.app-Header'
+});
+
+if (typeof window !== 'undefined') {
+  axios.interceptors.request.use((config) => {
+    NProgress.start();
+    return config;
+  });
+  axios.interceptors.response.use((response) => {
+    NProgress.done();
+    return response;
+  });
+}
 
 const http = {
   install(Vue) {
