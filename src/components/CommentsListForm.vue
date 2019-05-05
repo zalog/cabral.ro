178bdad3013efda5ec06db570db3ca4b70d413cb
Catalin Zalog <c@zalog.ro>
2019-05-05 16:50:22 +0300

    wip|feat(components): adds reply form markup
    - adds html markup
    - adds some animations

--- src/components/CommentsListForm.vue
+++ src/components/CommentsListForm.vue
@@ -1,6 +1,38 @@
 <template>
-  <form class="form-reply">
-    <div class="text-muted">{{ label }}</div>
+  <form
+    class="form-reply"
+    novalidate
+    @click="formOpen"
+    @submit.prevent="formSubmit"
+  >
+    <div class="label text-muted">{{ label }}</div>
+
+    <div class="form">
+      <div class="form-row">
+        <div class="form-group col-md-4">
+          <label for="form-reply-name" class="sr-only">Nume</label>
+          <input type="text" id="form-reply-name" class="form-control" placeholder="Nume" required>
+        </div>
+        <div class="form-group col-md-4">
+          <label for="form-reply-email" class="sr-only">Email</label>
+          <input type="email" id="form-reply-email" class="form-control" placeholder="Email" required>
+        </div>
+        <div class="form-group col-md-4">
+          <label for="form-reply-site" class="sr-only">Site</label>
+          <input type="url" id="form-reply-site" class="form-control" placeholder="Site">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="form-reply-message" class="sr-only">Mesaj</label>
+        <textarea id="form-reply-message" class="form-control" rows="4" placeholder="Mesajul tău..." required></textarea>
+      </div>
+
+      <button
+        v-ripple
+        type="submit"
+        class="btn btn-outline-secondary"
+      >Trimite</button>
+    </div>
   </form>
 </template>
 
@@ -13,18 +45,67 @@ export default {
       type: String,
       default: 'Lasă un comentariu...'
     }
+  },
+
+  methods: {
+    formOpen() {
+      if (this.$el.classList.contains('open')) return;
+
+      this.$el.classList.add('open');
+      document.body.addEventListener('click', this.formClose);
+    },
+    formClose(event) {
+      const selfClicked = event.target.closest('.form-reply') === this.$el;
+
+      if (selfClicked) return;
+
+      this.$el.classList.remove('open');
+      document.body.removeEventListener('click', this.formClose);
+    },
+    formSubmit() {
+      console.log('formSubmit');
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
 @import "./../scss/app-component.scss";
+@import "~bootstrap/scss/forms";
+@import "~bootstrap/scss/buttons";
 
 .form-reply {
   padding: map-get($spacers, 4);
   margin-bottom: map-get($spacers, 4);
   background-color: $white;
   box-shadow: $box-shadow-sm;
+
+  .label,
+  .form {
+    transition: max-height .2s ease, opacity .2s ease;
+    overflow: hidden;
+  }
+
+  .label {
+    max-height: 30px;
+  }
+
+  .form {
+    max-height: 0;
+    opacity: 0;
+  }
+
+  &.open {
+    .label {
+      max-height: 0;
+      opacity: 0;
+    }
+    .form {
+      max-height: 350px;
+      opacity: 1;
+      overflow: visible;
+    }
+  }
 }
 
 .list-comments {
