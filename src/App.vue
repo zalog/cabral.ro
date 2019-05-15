992b77303824273344a3d9d4b4c98689afda1ed5
Catalin Zalog <c@zalog.ro>
2019-05-15 22:04:28 +0300

    feat: adds gtm script

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,18 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' }
     ],
+    script: [{
+      vmid: 'gtm',
+      innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-59M55JB');`,
+      type: 'text/javascript'
+    }],
+    __dangerouslyDisableSanitizersByTagID: {
+      'gtm': ['innerHTML']
+    },
     titleTemplate: (titleChunk) => titleChunk ? `${titleChunk} - ${SITE.TITLE}` : SITE.TITLE
   },
 
