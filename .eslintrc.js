1b64399586cb07939a074ef6975ae77d5a734b19
Catalin Zalog <c@zalog.ro>
2019-12-01 14:30:53 +0200

    style: adds some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,10 @@ module.exports = {
         "no-console": "off",
         // other eslint
         "indent": ["error", 4],
+        "quotes": ["error", "single"],
+        "space-before-blocks": "error",
+        "keyword-spacing": ["error"],
+        "comma-dangle": ["error", "never"],
         // overwrite vue/essential
         "vue/mustache-interpolation-spacing": "error",
         "vue/html-indent": ["error", 4],
