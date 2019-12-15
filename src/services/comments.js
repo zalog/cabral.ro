37280633a1aea1d10f33066a1dc3b26b046c0b1e
Catalin Zalog <c@zalog.ro>
2019-12-15 21:44:37 +0200

    feat(services): adds async/await to `fetchComments`

--- src/services/comments.js
+++ src/services/comments.js
@@ -1,66 +1,64 @@
 import Vue from 'vue';
 import { ENDPOINTS } from './../utils/constants';
 
-export function fetchComments(payload) {
+export async function fetchComments(payload) {
     let params = {
         onPage: 10,
         after: null,
         ...payload
     };
 
-    return new Promise((resolve, reject) => {
-        Vue.prototype.$http({
-            method: 'post',
-            url: ENDPOINTS.GRAPHQL,
-            data: {
-                query: `
-                    query GET_COMMENTS_FOR_POST(
-                        $id: ID,
-                        $first: Int,
-                        $after: String
+    const response = await Vue.prototype.$http({
+        method: 'post',
+        url: ENDPOINTS.GRAPHQL,
+        data: {
+            query: `
+                query GET_COMMENTS_FOR_POST(
+                    $id: ID,
+                    $first: Int,
+                    $after: String
+                ) {
+                    comments(
+                        first: $first,
+                        after: $after,
+                        where: {contentId: $id}
                     ) {
-                        comments(
-                            first: $first,
-                            after: $after,
-                            where: {contentId: $id}
-                        ) {
-                            nodes {
-                                ...CommentFields
-                                replies: children {
-                                    nodes {
-                                        ...CommentFields
-                                    }
+                        nodes {
+                            ...CommentFields
+                            replies: children {
+                                nodes {
+                                    ...CommentFields
                                 }
                             }
-                            pageInfo {
-                                hasNextPage
-                                endCursor
-                            }
+                        }
+                        pageInfo {
+                            hasNextPage
+                            endCursor
                         }
                     }
-
-                    fragment CommentFields on Comment {
-                        commentId
-                        author {
-                            ... on CommentAuthor {
-                                name
-                                url
-                            }
-                        }
-                        date
-                        content
-                    }
-                `,
-                variables: {
-                    'id': params.singleId,
-                    'first': params.onPage,
-                    'after': params.after
                 }
+
+                fragment CommentFields on Comment {
+                    commentId
+                    author {
+                        ... on CommentAuthor {
+                            name
+                            url
+                        }
+                    }
+                    date
+                    content
+                }
+            `,
+            variables: {
+                'id': params.singleId,
+                'first': params.onPage,
+                'after': params.after
             }
-        })
-            .then((response) => resolve(response.data.data.comments))
-            .catch((error) => reject(error));
+        }
     });
+
+    return response.data.data.comments;
 }
 
 export function postComment(payload) {
