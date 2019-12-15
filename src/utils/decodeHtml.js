ea7d9dc7eee94685ddf086ebaa8fe55216e8cf19
Catalin Zalog <c@zalog.ro>
2019-12-15 15:30:31 +0200

    fix(utils): validates data

--- src/utils/decodeHtml.js
+++ src/utils/decodeHtml.js
@@ -1,4 +1,6 @@
 export default (string) => {
+    if (!string || typeof string !== 'string') return;
+
     return string.replace(
         /&#(\d+);/g,
         (match, dec) => String.fromCharCode(dec)
