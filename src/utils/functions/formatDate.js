5c505d4133d182f52abbb6fb9d263b16767fc468
Catalin Zalog <c@zalog.ro>
2019-12-21 16:56:25 +0200

    feat(utils): adds formatDate leading 0

--- src/utils/functions/formatDate.js
+++ src/utils/functions/formatDate.js
@@ -2,6 +2,12 @@ export function formatDate(string) {
     if (!string || typeof string !== 'string') return;
 
     const date = new Date(string);
+    const dd = getDD(date.getDate());
+    const mm = getMM(date.getMonth());
+    const yy = date.getFullYear();
 
-    return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+    return `${dd}.${mm}.${yy}`;
 }
+
+const getDD = (number) => number < 10 ? `0${number}` : number;
+const getMM = (number) => number + 1;
