5c0847ccaec3b8aebd83c66d7909fa2a6b2572a4
Catalin Zalog <c@zalog.ro>
2019-12-22 23:23:16 +0200

    fix(utils): makes links relative
    Fixes category navigation.

--- src/utils/adaptors/item-post.js
+++ src/utils/adaptors/item-post.js
@@ -1,7 +1,13 @@
+import { SITE } from './../constants';
+
 export default (payload) => {
     const payloadDate = new Date(payload.date);
     const payloadModified = new Date(payload.modified);
     const date = ((payloadModified > payloadDate) && payloadModified || payloadDate).toString();
+    const categories = payload.embed.categories.map(category => ({
+        ...category,
+        link: category.link.replace(SITE.LINK, '')
+    }));
 
     const output = {
         excerpt: payload.excerpt.rendered,
@@ -9,7 +15,7 @@ export default (payload) => {
         slug: payload.slug,
         title: payload.title.rendered,
         commentsNumber: payload.comments_number,
-        categories: payload.embed.categories,
+        categories,
         date
     };
 
