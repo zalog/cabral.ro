30c4fd72703bca8f450654ce27f23f6f41aa108b
Catalin Zalog <c@zalog.ro>
2019-12-24 01:41:03 +0200

    feat(utils): adds `getLeadingZero` function

--- src/utils/functions/formatDate.js
+++ src/utils/functions/formatDate.js
@@ -2,12 +2,12 @@ export function formatDate(string) {
     if (!string || typeof string !== 'string') return;
 
     const date = new Date(string);
-    const dd = getDD(date.getDate());
+    const dd = getLeadingZero(date.getDate());
     const mm = getMM(date.getMonth());
     const yy = date.getFullYear();
 
     return `${dd}.${mm}.${yy}`;
 }
 
-const getDD = (number) => number < 10 ? `0${number}` : number;
-const getMM = (number) => number + 1;
+const getLeadingZero = (number) => number < 10 ? `0${number}` : number;
+const getMM = (number) => getLeadingZero(number + 1);
