9fa7dc56096c6ff88ee6bda7adff5374c40d1a0d
Catalin Zalog <c@zalog.ro>
2019-12-25 17:18:13 +0200

    feat(pages): brings `featuredMedia` on Single

--- src/utils/adaptors/single-post.js
+++ src/utils/adaptors/single-post.js
@@ -11,6 +11,7 @@ export default (payload) => {
 
     const output = {
         id: payload.id,
+        featuredMedia: payload.featured_media,
         title: payload.title.rendered,
         content: payload.content,
         commentsNumber: payload.comments_number,
