477eb183b50831865e9494632ef22035b59fae51
Catalin Zalog <c@zalog.ro>
2019-12-25 11:51:18 +0200

    feat(utils): returns output
    Prepares for some data manipulation.

--- src/utils/adaptors/single-post.js
+++ src/utils/adaptors/single-post.js
@@ -1,6 +1,10 @@
-export default (payload) => ({
-    id: payload.id,
-    title: payload.title.rendered,
-    content: payload.content,
-    commentsNumber: payload.comments_number
-});
+export default (payload) => {
+    const output = {
+        id: payload.id,
+        title: payload.title.rendered,
+        content: payload.content,
+        commentsNumber: payload.comments_number
+    };
+
+    return output;
+};
