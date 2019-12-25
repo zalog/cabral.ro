ee68a365acc490e6863fee6efd9bbfb37f02ce6a
Catalin Zalog <c@zalog.ro>
2019-12-25 12:10:52 +0200

    feat(pages): adds item info

--- src/utils/adaptors/single-post.js
+++ src/utils/adaptors/single-post.js
@@ -1,9 +1,21 @@
+import { SITE } from './../constants';
+
 export default (payload) => {
+    const payloadDate = new Date(payload.date);
+    const payloadModified = new Date(payload.modified);
+    const date = ((payloadModified > payloadDate) && payloadModified || payloadDate).toString();
+    const categories = payload.embed.categories.map(category => ({
+        ...category,
+        link: category.link.replace(SITE.LINK, '')
+    }));
+
     const output = {
         id: payload.id,
         title: payload.title.rendered,
         content: payload.content,
-        commentsNumber: payload.comments_number
+        commentsNumber: payload.comments_number,
+        categories,
+        date
     };
 
     return output;
