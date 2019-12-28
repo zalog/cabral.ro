8c6a573718c0293a4ed57c68d54864b4a996b1e9
Catalin Zalog <c@zalog.ro>
2019-12-28 13:09:18 +0200

    feat(app): returns `output`
    Preparing for some data manipulation.

--- src/App.vue
+++ src/App.vue
@@ -30,27 +30,31 @@ export default {
         }
     },
 
-    metaInfo: {
-        htmlAttrs: {
-            lang: SITE.LANG
-        },
-        titleTemplate: (titleChunk) => SITE.TITLE_TEMPLATE(titleChunk),
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' }
-        ],
-        script: [{
-            vmid: 'gtm',
-            innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${SITE.GTM_ID}');`,
-            type: 'text/javascript'
-        }],
-        __dangerouslyDisableSanitizersByTagID: {
-            'gtm': ['innerHTML']
-        }
+    metaInfo() {
+        const output = {
+            htmlAttrs: {
+                lang: SITE.LANG
+            },
+            titleTemplate: (titleChunk) => SITE.TITLE_TEMPLATE(titleChunk),
+            meta: [
+                { charset: 'utf-8' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' }
+            ],
+            script: [{
+                vmid: 'gtm',
+                innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${SITE.GTM_ID}');`,
+                type: 'text/javascript'
+            }],
+            __dangerouslyDisableSanitizersByTagID: {
+                'gtm': ['innerHTML']
+            }
+        };
+
+        return output;
     }
 };
 </script>
