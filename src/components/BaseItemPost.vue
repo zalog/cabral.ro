4791c9eb84c5a2342859e5c5a8de7127e690bda8
Catalin Zalog <c@zalog.ro>
2019-12-29 15:20:33 +0200

    feat(components): adds BaseItemPost

---
+++ src/components/BaseItemPost.vue
@@ -0,0 +1,146 @@
+<template>
+    <router-link
+        tag="div"
+        class="card mb-5"
+        v-ripple
+        :to="postTo(post.slug)"
+    >
+        <div
+            v-if="post.featuredMedia"
+            class="card-img-top"
+        >
+            <div class="img" v-html="post.featuredMedia" />
+            <ul class="list-img-info">
+                <li>
+                    <base-icon name="comment" class="icon-sm" />
+                    {{ post.commentsNumber }}
+                </li>
+            </ul>
+        </div>
+        <div class="card-body" :to="postTo(post.slug)">
+            <h2 class="card-title">
+                <router-link v-html="post.title" :to="postTo(post.slug)" />
+            </h2>
+            <list-item-info
+                :data="[{
+                    icon: 'date',
+                    text: $options.filters.formatDate(post.date)
+                }, {
+                    icon: 'folder',
+                    links: post.categories
+                }]"
+            />
+            <div class="card-text" v-html="post.excerpt" />
+        </div>
+    </router-link>
+</template>
+
+<script>
+import ListItemInfo from './ListItemInfo.vue';
+
+export default {
+    name: 'BaseItemPost',
+
+    components: {
+        ListItemInfo
+    },
+
+    props: {
+        post: {
+            type: Object,
+            required: true
+        }
+    },
+
+    methods: {
+        postTo(slug) {
+            return {
+                name: 'single',
+                params: {
+                    singleSlug: slug,
+                    singleType: 'post'
+                }
+            };
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./../scss/app-component.scss";
+
+$card-border-radius:          $border-radius-lg;
+$card-border-color:           transparent;
+
+@import "~bootstrap/scss/card";
+
+.img {
+    position: relative;
+    padding-top: percentage(9/16);
+    overflow: hidden;
+
+    /deep/ {
+        img {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 100%;
+            height: auto;
+        }
+    }
+}
+
+.list-img-info {
+    @include list-unstyled;
+    display: flex;
+
+    li {
+        padding: $badge-padding-y * 1.5 $badge-padding-x * 2;
+        font-size: $badge-font-size;
+        border-radius: $badge-border-radius;
+        background-color: rgba($white, .6);
+        color: $gray-800;
+    }
+
+    li + li {
+        margin-left: $spacer;
+    }
+}
+
+.card-text {
+    /deep/ {
+        > p a {
+            display: none;
+        }
+
+        > *:last-child {
+            margin-bottom: 0;
+        }
+    }
+}
+
+.card {
+    cursor: pointer;
+    box-shadow: $box-shadow-lg;
+    border: 0;
+
+    .card-img-top {
+        position: relative;
+        overflow: hidden;
+
+        .list-img-info {
+            position: absolute;
+            top: $spacer;
+            right: $spacer;
+        }
+    }
+    .card-title > a {
+        color: $body-color;
+    }
+    .list-item-info {
+        margin-top: -(map-get($spacers, 2));
+    }
+}
+
+</style>
--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -3,41 +3,12 @@
         class="cards-posts"
         v-if="posts.data.length"
     >
-        <router-link
-            tag="div"
-            class="card mb-5"
-            v-ripple
-            v-for="(post, index) in posts.data" :key="'posts-post-' + index"
-            :to="postTo(post.slug)"
-        >
-            <div
-                v-if="post.featuredMedia"
-                class="card-img-top"
-            >
-                <div class="img" v-html="post.featuredMedia" />
-                <ul class="list-img-info">
-                    <li>
-                        <base-icon name="comment" class="icon-sm" />
-                        {{ post.commentsNumber }}
-                    </li>
-                </ul>
-            </div>
-            <div class="card-body" :to="postTo(post.slug)">
-                <h2 class="card-title">
-                    <router-link v-html="post.title" :to="postTo(post.slug)" />
-                </h2>
-                <list-item-info
-                    :data="[{
-                        icon: 'date',
-                        text: $options.filters.formatDate(post.date)
-                    }, {
-                        icon: 'folder',
-                        links: post.categories
-                    }]"
-                />
-                <div class="card-text" v-html="post.excerpt" />
-            </div>
-        </router-link>
+        <template v-for="(post, index) in posts.data">
+            <base-item-post
+                :key="`post-${index}`"
+                :post="post"
+            />
+        </template>
 
         <nav aria-label="Page navigation">
             <ul class="pagination justify-content-center">
@@ -61,13 +32,13 @@
 
 <script>
 import './../utils/filters/formatDate';
-import ListItemInfo from './ListItemInfo.vue';
+import BaseItemPost from './BaseItemPost.vue';
 
 export default {
     name: 'PostsList',
 
     components: {
-        ListItemInfo
+        BaseItemPost
     },
 
     props: {
@@ -86,15 +57,6 @@ export default {
             if (routeCategory) url = `/category/${routeCategory}${url}`;
 
             return url;
-        },
-        postTo(slug) {
-            return {
-                name: 'single',
-                params: {
-                    singleSlug: slug,
-                    singleType: 'post'
-                }
-            };
         }
     }
 };
@@ -102,9 +64,6 @@ export default {
 
 <style lang="scss" scoped>
 @import "./../scss/app-component.scss";
-
-$card-border-radius:          $border-radius-lg;
-$card-border-color:           transparent;
 
 $pagination-color:            $body-color;
 $pagination-bg:               transparent;
@@ -115,76 +74,9 @@ $pagination-active-color:     $pagination-hover-color;
 $pagination-active-bg:        $gray-100;
 $pagination-focus-box-shadow: none;
 
-@import "~bootstrap/scss/card";
 @import "~bootstrap/scss/pagination";
 
-.cards-posts /deep/ {
-    .img {
-        position: relative;
-        padding-top: percentage(9/16);
-        overflow: hidden;
-
-        img {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            width: 100%;
-            height: auto;
-        }
-    }
-
-    .card-text > {
-        p a {
-            display: none;
-        }
-
-        *:last-child {
-            margin-bottom: 0;
-        }
-    }
-}
-
-.list-img-info {
-    @include list-unstyled;
-    display: flex;
-
-    li {
-        padding: $badge-padding-y * 1.5 $badge-padding-x * 2;
-        font-size: $badge-font-size;
-        border-radius: $badge-border-radius;
-        background-color: rgba($white, .6);
-        color: $gray-800;
-    }
-
-    li + li {
-        margin-left: $spacer;
-    }
-}
-
 .cards-posts {
-    .card {
-        cursor: pointer;
-        box-shadow: $box-shadow-lg;
-        border: 0;
-    }
-    .card-img-top {
-        position: relative;
-        overflow: hidden;
-
-        .list-img-info {
-            position: absolute;
-            top: $spacer;
-            right: $spacer;
-        }
-    }
-    .card-title > a {
-        color: $body-color;
-    }
-    .list-item-info {
-        margin-top: -(map-get($spacers, 2));
-    }
-
     .pagination {
         .active {
             font-weight: bold;
