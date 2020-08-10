aebb8fe914dd10ea8d11286640142b04ed0f9afd
Catalin Zalog <c@zalog.ro>
2020-08-10 08:16:31 +0300

    style(utils): renames adaptors

--- src/utils/adaptors/index.js
+++ src/utils/adaptors/index.js
@@ -1,4 +1,4 @@
 export { default as itemPost } from './item-post';
 export { default as itemCategory } from './item-category';
-export { default as singlePost } from './single-post';
-export { default as singlePage } from './single-page';
+export { default as pageSinglePost } from './page-single-post';
+export { default as pageSinglePage } from './page-single-page';
