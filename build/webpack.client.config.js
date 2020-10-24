82638f33f7ac3753739745d6c1ac2edaa4298a2c
Catalin Zalog <c@zalog.ro>
2020-10-24 10:36:11 +0300

    fix: removes esModule in MiniCssExtractPlugin
    Fixes `"export 'default' (imported as 'mod') was not found` warning.

--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -19,6 +19,7 @@ const config = merge(base, {
                     {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
+                            esModule: false,
                             hmr: !isProd,
                             reloadAll: !isProd
                         }
