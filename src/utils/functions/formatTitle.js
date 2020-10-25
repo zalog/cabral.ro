b6077a92fcaec76f9b1671e55bc4a81cabd830ba
Catalin Zalog <c@zalog.ro>
2020-10-25 09:31:25 +0200

    feat(utils): removes not needed reducer

--- src/utils/functions/formatTitle.js
+++ src/utils/functions/formatTitle.js
@@ -3,13 +3,13 @@ import { SITE } from './../constants';
 export function formatHtmlTitle(title) {
     if (!title) return SITE.TITLE;
 
-    const separator = '-';
+    const separator = ' - ';
 
     if (typeof title === 'string') title = [title];
 
     title.push(SITE.TITLE);
 
-    return title.reduce((acc, cur) => `${acc} ${separator} ${cur}`);
+    return title.join(separator);
 }
 
 export function formatPageTitle(title) {
