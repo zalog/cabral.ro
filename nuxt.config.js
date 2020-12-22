7f185189f722cec0fc1c1359f3e471128e4e0e33
Catalin Zalog <c@zalog.ro>
2020-12-22 08:53:23 +0200

    feat: adds axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ export default {
     serverMiddleware: ['~/middleware/server/spa.js'],
     components: true,
     modules: [
+        '@nuxtjs/axios',
         '@nuxtjs/svg-sprite'
     ],
     svgSprite: {
