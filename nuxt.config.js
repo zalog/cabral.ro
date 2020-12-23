7fd1f195a7a2455887661f8049d6849d30843f07
Catalin Zalog <c@zalog.ro>
2020-12-23 20:38:15 +0200

    feat: adds homepage routes & page

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,22 @@
 export default {
     serverMiddleware: ['~/middleware/server/spa.js'],
     components: true,
+    router: {
+        extendRoutes(routes, resolve) {
+            routes.push(
+                {
+                    path: '/page/1/',
+                    redirect: '/'
+                },
+                {
+                    name: 'page-id',
+                    path: '/page/:id?',
+                    component: resolve(__dirname, 'pages/index.vue'),
+                    chunkName: 'pages/index'
+                }
+            );
+        }
+    },
     css: [
         '~/assets/scss/app.scss'
     ],
