32b4722d24ecfcdca480c45973489a38fbfdd5a9
Catalin Zalog <c@zalog.ro>
2020-12-25 08:55:32 +0200

    style: applies default order

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,25 +21,7 @@ module.exports = {
         'vue/mustache-interpolation-spacing': 'error',
         'vue/html-indent': ['error', 4],
         'vue/script-indent': ['error', 4],
-        'vue/order-in-components': ['error', {
-            'order': [
-                'el', // vue side effects
-                'name', 'parent', // vue global awareness
-                'functional', // vue component type
-                ['delimiters', 'comments'], // vue template modifiers
-                ['components', 'directives', 'filters'], // vue template dependencies
-                'extends', 'mixins', // vue composition
-                'beforeRouteEnter', 'beforeRouteUpdate', 'beforeRouteLeave', // vue router
-                'inheritAttrs', 'model', ['props', 'propsData'], // vue interface
-                'data', 'computed', // vue local state
-                'watch', // vue events
-                'serverPrefetch', // vue ssr
-                'LIFECYCLE_HOOKS', // vue events
-                'methods', // vue non-reactive properties
-                'metaInfo', // vue-meta
-                ['template', 'render'], 'renderError' // vue rendering
-            ]
-        }]
+        'vue/order-in-components': ['error']
     },
     'parserOptions': {
         'parser': 'babel-eslint'
