a5beb0074b766035ca4f9f6292a51249958e5805
Catalin Zalog <c@zalog.ro>
2020-12-30 22:17:28 +0200

    feat: moves store pages to object

--- store/data.js
+++ store/data.js
@@ -4,17 +4,15 @@ import { formatPageTitle } from '~/utils';
 
 const pagesToKeep = 5;
 
-export const state = () => ([]);
+export const state = () => ({});
 
 export const getters = {
     currentPage: (state) => (path, passedState) => {
         state = passedState || state;
 
         const pages = state;
-        const page = pages.find(page => page[path]);
-        const output = typeof page !== 'undefined' && page[path] || false;
-
-        return output;
+        const page = pages[path];
+        return page || false;
     },
     currentPageTitle: (state, getters) => () => {
         const page = getters.currentPage();
@@ -32,29 +30,28 @@ export const mutations = {
         const dataKey = payload.prop;
         let data = null;
 
-        let page = state.find(page => page[key]);
-        if (!page) {
-            state.push({
-                [key]: {}
-            });
-            page = state[state.length - 1];
-        }
+        if (!state[key]) Vue.set(state, key, {});
 
-        const pageData = page[key];
-        if (pageData instanceof Array) {
-            data = [ ...pageData, ...payload.data ];
-        } else if (pageData instanceof Object) {
-            data = { ...pageData, ...payload.data };
+        const page = state[key];
+
+        if (page instanceof Array) {
+            data = [ ...page, ...payload.data ];
+        } else if (page instanceof Object) {
+            data = { ...page, ...payload.data };
         } else {
             data = payload.data;
         }
 
-        Vue.set(pageData, dataKey, data);
-        Vue.set(pageData, 'timestamp', {
-            ...pageData.timestamp,
+        Vue.set(page, dataKey, data);
+        Vue.set(page, 'timestamp', {
+            ...page.timestamp,
             [dataKey]: timestamp
         });
 
-        if (state.length > pagesToKeep) state.shift();
+        if (Object.keys(state).length > pagesToKeep) {
+            const keyToDelete = Object.keys(state).reduce((prev, curr) =>
+                state[prev].timestamp.main < state[curr].timestamp.main ? prev : curr);
+            delete state[keyToDelete];
+        }
     }
 };
