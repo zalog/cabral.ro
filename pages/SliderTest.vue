1af13609d4f964bef73e7cdcee8bc1da3205fed6
Catalin Zalog <c@zalog.ro>
2021-04-28 22:09:07 +0200

    feat: prepares test page

---
+++ pages/SliderTest.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container-fluid">
+        <h1>Slider test {{ $route.params.id }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: 'slider-test',
+};
+</script>
