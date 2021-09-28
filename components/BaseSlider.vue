c5f73774049c72441b742b9b88b48e9834c1d152
Catalin Zalog <c@zalog.ro>
2021-09-28 20:28:36 +0300

    feat: moves base-slider css component

--- components/BaseSlider.vue
+++ components/BaseSlider.vue
@@ -400,7 +400,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import "~/assets/scss/05-components/slider";
+@import "~/assets/scss/05-components/base-slider";
 
 .debugging {
     margin-top: 20px;
