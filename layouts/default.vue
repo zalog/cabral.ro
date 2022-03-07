42b4d17fe64b94c58da14cc903090b876fa5ea63
Catalin Zalog <c@zalog.ro>
2022-03-07 07:08:15 +0200

    feat: adds empty sidebars

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="layout-default">
         <the-header />
-        <Nuxt class="page" />
+        <div class="container-fluid">
+            <div class="row no-gutters">
+                <div class="d-none d-xl-block col-xl-3 sidebar-left" />
+                <Nuxt class="col-12 col-xl-6 page" />
+                <div class="d-none d-xl-block col-xl-3 sidebar-right" />
+            </div>
+        </div>
         <lazy-the-notifications />
     </div>
 </template>
