9351202631567fd36aab8ba31caa1bbfa0c394d4
Catalin Zalog <c@zalog.ro>
2022-03-12 06:34:22 +0200

    feat: renames vuex module

--- plugins/window.js
+++ plugins/window.js
@@ -20,29 +20,29 @@ const isBreakpoint = ({ breakpointNeeded, breakpointCurrent, direction = 'up' })
 
 export default ({ app, store }, inject) => {
     inject('mediaBreakpoint', (breakpoint) => {
-        if (breakpoint === store.state.ui.window.breakpoint) return true;
+        if (breakpoint === store.state.ui.client.breakpoint) return true;
 
         return false;
     });
 
     inject('mediaBreakpointUp', (breakpoint) => isBreakpoint({
         breakpointNeeded: breakpoint,
-        breakpointCurrent: store.state.ui.window.breakpoint,
+        breakpointCurrent: store.state.ui.client.breakpoint,
         direction: 'up',
     }));
 
     inject('mediaBreakpointDown', (breakpoint) => isBreakpoint({
         breakpointNeeded: breakpoint,
-        breakpointCurrent: store.state.ui.window.breakpoint,
+        breakpointCurrent: store.state.ui.client.breakpoint,
         direction: 'down',
     }));
 
     Object.assign(app, {
         mounted() {
-            store.dispatch('ui/window/attachResizeObserve', {
+            store.dispatch('ui/client/attachResizeObserve', {
                 el: app.router.app.$el,
             });
-            store.dispatch('ui/window/attachScroll');
+            store.dispatch('ui/client/attachScroll');
         },
     });
 };
