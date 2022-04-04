80dadd62b761f17a179f2a8ecb07da558273079c
Catalin Zalog <c@zalog.ro>
2022-04-04 07:37:47 +0300

    feat: removes sidebar-right

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,11 +3,10 @@
         <the-header />
         <div class="container-fluid">
             <div class="row no-gutters">
-                <div class="d-none d-xl-block col-xl-3 sidebar-left">
+                <div class="d-none d-xl-block col-xl-3 sidebar">
                     <portal-target name="widget-categories" />
                 </div>
                 <Nuxt class="col-12 col-xl-6 page" />
-                <div class="d-none d-xl-block col-xl-3 sidebar-right" />
             </div>
         </div>
         <lazy-the-notifications />
