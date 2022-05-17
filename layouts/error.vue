8e15d44e3404411e95486f48ac0e2e2d3306f6ee
Catalin Zalog <c@zalog.ro>
2022-05-17 08:07:51 +0300

    feat: adds custom error page

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>
+            <template v-if="error.statusCode === 404">
+                {{ error.message || 'Page not found' }}
+            </template>
+            <template v-else>
+                An error occurred
+            </template>
+        </h1>
+        <p>
+            Go to
+            <nuxt-link to="/">
+                home page
+            </nuxt-link>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        error: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
