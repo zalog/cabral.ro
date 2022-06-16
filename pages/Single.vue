d6cb236b26c45cdc9c302fb43be5cd9d4947f03f
Catalin Zalog <c@zalog.ro>
2022-06-16 07:51:36 +0300

    feat: pass already processed page title
    Title processed by `formatHtmlTitle` needed. This keeps `-` title separator.

--- pages/Single.vue
+++ pages/Single.vue
@@ -57,7 +57,7 @@
             v-if="galleryFull.show"
             v-model="galleryFull.index"
             :items="galleryFull.items"
-            :page-title="pageTitle"
+            :page-title="$metaInfo.title"
             @hidden="galleryFull.show = false"
         />
     </div>
